09e9a82627c6c2ddef48454b96c13143446c4c8f
Bruno Silvestre <143884382+75895@users.noreply.github.com>
2025-10-16 22:32:38 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// Arquivo: /home/ubuntu/restaurante_system/frontend/js/config.js
-
-const API_BASE_URL = "https://restaurante-system.onrender.com";
-
-// Exporta a URL para uso em outros módulos JavaScript
-export { API_BASE_URL };
