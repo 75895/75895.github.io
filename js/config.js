f611dc70a9e925f0e3c5fd600c374387ea932dd6
Bruno Silvestre <143884382+75895@users.noreply.github.com>
2025-10-16 22:30:13 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,6 @@
+// Arquivo: /home/ubuntu/restaurante_system/frontend/js/config.js
+
+const API_BASE_URL = "https://restaurante-system.onrender.com";
+
+// Exporta a URL para uso em outros módulos JavaScript
+export { API_BASE_URL };
